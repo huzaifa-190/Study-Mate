54fe4e7767eb9f46c06b01d17c831a67286c50b7
huzaifa190 <160710050+huzaifa-190@users.noreply.github.com>
2025-07-02 05:59:39 +0500

    FIX: Fixed charts with real data

--- src/pages/Dashboard/ECommerce.tsx
+++ src/pages/Dashboard/ECommerce.tsx
@@ -1,21 +1,16 @@
 import React from 'react';
-import ChartOne from '../../components/Charts/ChartOne';
 import ChartTwo from '../../components/Charts/ChartTwo';
 import ChartThree from '../../components/Charts/ChartThree';
 
 const ECommerce: React.FC = () => {
   return (
-    <>
-      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+    
+      <div className="grid w-screen xl:w-[80%] grid-cols-1 gap-4 lg:grid-cols-2 xl:grid-cols-2 2xl:grid-cols-2  ">
+        <ChartTwo />
+        <ChartThree />
       </div>
 
-      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5 mx-auto">
-        {/* <ChartOne /> */}
-        <ChartTwo />  
-        <ChartThree />
-        {/* <MapOne /> */}
-      </div>
-    </>
+    
   );
 };
 
